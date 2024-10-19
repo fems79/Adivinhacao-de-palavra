3b0f2f0d5249ab763a435cc1f20b2f44664f3c72
leocaricchio <leocaricchio@academico.ufs.br>
2024-10-19 17:49:32 -0300

    Update listas.js

--- listas.js
+++ listas.js
@@ -1,4 +1,4 @@
 // Os elementos da lista que vâo ser sorteados de acordo com o tema
 const Pais = ["Haiti" ,"Chile" ,"Aruba" ,"Egito" ,"Chade" ,"Tonga" ,"Samoa" ,"Palau" ,"Nauru" ,"Malta" ,"Aland", "China" ,"Catar" ,"Nepal" ,"Macau" ]
 const Pokemon = ["golem" , "goldy" , "ekans" , "arbok" , "zubat" , "gloom", "paras" , "doduo" , "hypno" , "ditto" , "pichu" , "aipom"]
-//const 
+const Times_de_Futebol=["Bahia","sport","vasco","parma","oeste","goias","bangu","Ceara","Porto","Milan","River","Lazio","Lille","Mainz","Celta","Gijon","Malmo","Tigre","Legia","Genoa","Velez","apoel"]
