90cdb4e5a1cd055bc6b24ab4115f9c8eda38602c
Felipe Ramos Santana <felipe.ramos@dcomp.ufs.br>
2024-10-21 21:17:59 -0300

    Adição de novas listas
    Adição de novas listas para os novos temas

--- listas.js
+++ listas.js
@@ -2,3 +2,7 @@
 const Pais = ["Haiti" ,"Chile" ,"Aruba" ,"Egito" ,"Chade" ,"Tonga" ,"Samoa" ,"Palau" ,"Nauru" ,"Malta" ,"Aland", "China" ,"Catar" ,"Nepal" ,"Macau" ]
 const Pokemon = ["golem" , "goldy" , "ekans" , "arbok" , "zubat" , "gloom", "paras" , "doduo" , "hypno" , "ditto" , "pichu" , "aipom"]
 const Times_de_Futebol=["Bahia","sport","vasco","parma","oeste","goias","bangu","Ceara","Porto","Milan","River","Lazio","Lille","Mainz","Celta","Gijon","Malmo","Tigre","Legia","Genoa","Velez","apoel"]
+const Animais=['Tigre','Cobra','Polvo','Ostra','Ácaro']
+const Frutas=['Cacau','Caqui','Limao','Manga','Melao','Amora','Nozes','Pinha']
+const Filmes=['Rambo','Rocky','Alien','Corra','Gilda','Creed','Drive','Belle','Crash','Glory']
+const Series=['Dexter','Arrow','Bones','Chuck','Ozark','Haven','Suits','Fringe','Merlí','Twins','Fargo','Barry']
